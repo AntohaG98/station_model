772b54d51716ebb38c8c3c98e4a8c180b373b907
Kochkins <s16247@yandex.ru>
2019-11-23 23:48:28 +0300

    Добавил переменную с текущим временем

--- lib/time_module.py
+++ lib/time_module.py
@@ -9,6 +9,7 @@ class TimeGen:
         self.arriving = False  # поезд приехал
         self.boarding = False  # началась посадка
         self.left = False  # поезд уехал
+        self.sim_time = 18000  # 5 часов
 
     sleeps = [600, 2400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 2400, 2400, 2400,
               2400, 2400, 2400, 2400, 600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 600, 600,
@@ -18,16 +19,20 @@ class TimeGen:
         # этот блок описывает события вокруг каждого поезда
         self.arriving_soon = True
         time.sleep(300 * self.speed)
+        self.sim_time += 300
         self.arriving = True
         time.sleep(300 * self.speed)
+        self.sim_time += 300
         self.boarding = True
         time.sleep(600 * self.speed)
+        self.sim_time += 600
         self.left = True
 
     def gen_time(self):
         # симуляция начинается в 5:00
         for i in range(len(self.sleeps)):
             time.sleep(self.sleeps[i] * self.speed)
+            self.sim_time += self.sleeps[i]
             self._train()
         time.sleep(1800 * self.speed)
         # симуляция заканчивается в 23:30
