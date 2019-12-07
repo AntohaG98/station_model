0399d66641afe2d787fabbc7d26fd4a834668405
zenger-anna <azenger98@gmail.com>
2019-12-07 20:40:57 +0300

    Сделала запись в excel

--- lib/provider_module.py
+++ lib/provider_module.py
@@ -1,4 +1,5 @@
-from time import sleep
+import openpyxl
+from datetime import datetime
 from threading import Thread
 
 
@@ -6,9 +7,16 @@ class StationProvider(Thread):
 
     # daemon = True
     train_capacity = [2000, 800, 1200]
+    column_names = ['Время', 'Событие', 'Пришли', 'Зал ожидания', 'Платформа 1', 'Платформа 2', 'Платформа 3',
+                    'Поезд 1', 'Поезд 2', 'Поезд 3']
 
     def __init__(self, gui_mod):
         Thread.__init__(self, target=self.main_loop)
+        self.excel_file = openpyxl.Workbook()
+        self.sheet = self.excel_file.create_sheet('Sheet_1', 0)
+        for i in range(1, 11):
+            self.sheet.cell(row=1, column=i).value = self.column_names[i - 1]
+        self.row_num = 2
         self.gui_mod = gui_mod
         self.alive = True
         self.people_dict = {
@@ -38,6 +46,26 @@ class StationProvider(Thread):
         self.minutes = 0
         self.speed = 1 / 100            # 100 менять на нужную
 
+    def update_info(self, message):
+        min_ = str(self.minutes)
+        if len(min_) == 1:
+            min_ = '0' + min_
+        time = ''.join([str(self.hours), ':', min_])
+        self.gui_mod.time_label.config(text=time)
+        self.gui_mod.info_label.config(text=message)
+        if self.gui_mod.file_var.get():
+            self.sheet.cell(row=self.row_num, column=1).value = time
+            self.sheet.cell(row=self.row_num, column=2).value = message
+
+    def save_station_state(self):
+        self.sheet.cell(row=self.row_num, column=4).value = str(self.people_dict['waiting_hall'])
+        self.sheet.cell(row=self.row_num, column=5).value = str(self.people_dict['platform_1'])
+        self.sheet.cell(row=self.row_num, column=6).value = str(self.people_dict['platform_2'])
+        self.sheet.cell(row=self.row_num, column=7).value = str(self.people_dict['platform_3'])
+        self.sheet.cell(row=self.row_num, column=8).value = str(self.people_dict['train_1'])
+        self.sheet.cell(row=self.row_num, column=9).value = str(self.people_dict['train_2'])
+        self.sheet.cell(row=self.row_num, column=10).value = str(self.people_dict['train_3'])
+
     def main_loop(self):
         while self.alive:
             if self.people_came and self.alive:
@@ -50,21 +78,26 @@ class StationProvider(Thread):
             if self.boarding and self.alive:
                 self.boarding_func()
             if self.left and self.alive:
+                self.people_dict['train_1'] = 0
+                self.people_dict['train_2'] = 0
+                self.people_dict['train_3'] = 0
                 print(self.hours, ":", self.minutes,
                       '\nПоезд уехал')
-                min_ = str(self.minutes)
-                if min_ == '0':
-                    min_ = '00'
-                time = ''.join([str(self.hours), ':', min_])
-                self.gui_mod.time_label.config(text=time)
-                self.gui_mod.info_label.config(text='Поезд уехал')
+                self.update_info('Поезд уехал')
                 self.gui_mod.train_1.config(text='', bg='lavender')
                 self.gui_mod.train_2.config(text='', bg='lavender')
                 self.gui_mod.train_3.config(text='', bg='lavender')
+
+                if self.gui_mod.file_var.get():
+                    self.save_station_state()
+                    self.row_num += 1
+
                 self.left = False
 
     def people_came_func(self):
         # обработка новых людей
+        if self.gui_mod.file_var.get():
+            self.sheet.cell(row=self.row_num, column=3).value = self.people_num
         if self.no_train and self.alive:
             wh = round(self.people_num * 0.66)
             p_to_platform = self.people_num - wh
@@ -82,10 +115,15 @@ class StationProvider(Thread):
             self.people_dict['platform_3'] += self.people_num - pl_1 - pl_2
 
         # to gui
+        self.update_info('Пришли люди')
         self.gui_mod.wait_num.config(text=str(self.people_dict['waiting_hall']))
         self.gui_mod.pl_1.config(text=str(self.people_dict['platform_1']))
         self.gui_mod.pl_2.config(text=str(self.people_dict['platform_2']))
         self.gui_mod.pl_3.config(text=str(self.people_dict['platform_3']))
+
+        if self.gui_mod.file_var.get():
+            self.save_station_state()
+            self.row_num += 1
 
         print('\nПришли люди. Загруженность вокзала:'
               '\n     - зал ожидания: ', self.people_dict['waiting_hall'],
@@ -96,12 +134,7 @@ class StationProvider(Thread):
 
     def arriving_func(self):
         # to gui
-        min_ = str(self.minutes)
-        if min_ == '0':
-            min_ = '00'
-        time = ''.join([str(self.hours), ':', min_])
-        self.gui_mod.time_label.config(text=time)
-        self.gui_mod.info_label.config(text='Приехали поезда')
+        self.update_info('Приехали поезда')
         self.gui_mod.wait_num.config(text=str(self.people_dict['waiting_hall']))
         self.gui_mod.pl_1.config(text=str(self.people_dict['platform_1']))
         self.gui_mod.pl_2.config(text=str(self.people_dict['platform_2']))
@@ -109,6 +142,10 @@ class StationProvider(Thread):
         self.gui_mod.train_1.config(text=str(self.people_dict['train_1']), bg='navy', fg='snow')
         self.gui_mod.train_2.config(text=str(self.people_dict['train_2']), bg='navy', fg='snow')
         self.gui_mod.train_3.config(text=str(self.people_dict['train_3']), bg='navy', fg='snow')
+
+        if self.gui_mod.file_var.get():
+            self.save_station_state()
+            self.row_num += 1
 
         print(self.hours, ":", self.minutes,
               '\nПриехали поезда:'
@@ -133,8 +170,8 @@ class StationProvider(Thread):
 
             # to gui
             min_ = str(self.minutes)
-            if min_ == '0':
-                min_ = '00'
+            if len(min_) == 1:
+                min_ = '0' + min_
             time = ''.join([str(self.hours), ':', min_])
             self.gui_mod.time_label.config(text=time)
             self.gui_mod.wait_num.config(text=str(self.people_dict['waiting_hall']))
@@ -152,6 +189,12 @@ class StationProvider(Thread):
                   '\n     - поезд 2: ', self.people_dict['train_2'],
                   '\n     - поезд 3: ', self.people_dict['train_3']
                   )
+
+            if self.gui_mod.file_var.get():
+                self.sheet.cell(row=self.row_num, column=1).value = time
+                self.sheet.cell(row=self.row_num, column=2).value = 'Люди из поездов ушли'
+                self.save_station_state()
+                self.row_num += 1
 
             # люди из зала идут на платформы
             if self.people_dict['waiting_hall'] != 0 and self.alive:
@@ -176,6 +219,12 @@ class StationProvider(Thread):
                   '\n     - платформа 3: ', self.people_dict['platform_3']
                   )
 
+            if self.gui_mod.file_var.get():
+                self.sheet.cell(row=self.row_num, column=1).value = time
+                self.sheet.cell(row=self.row_num, column=2).value = 'Люди из зала ожидания пошли на платформы'
+                self.save_station_state()
+                self.row_num += 1
+
             # люди садятся в поезд
         for i in range(0, 3):
             if not self.alive:
@@ -190,12 +239,7 @@ class StationProvider(Thread):
                     elif self.alive:
                         self.people_dict[''.join(['train_', str(i + 1)])] += free
                         self.people_dict[''.join(['platform_', str(i + 1)])] -= free
-                    min_ = str(self.minutes)
-                    if min_ == '0':
-                        min_ = '00'
-                    time = ''.join([str(self.hours), ':', min_])
-                    self.gui_mod.time_label.config(text=time)
-                    self.gui_mod.info_label.config(text='Посадка')
+                    self.update_info('Посадка')
                     self.gui_mod.wait_num.config(text=str(self.people_dict['waiting_hall']))
                     self.gui_mod.pl_1.config(text=str(self.people_dict['platform_1']))
                     self.gui_mod.pl_2.config(text=str(self.people_dict['platform_2']))
@@ -203,6 +247,11 @@ class StationProvider(Thread):
                     self.gui_mod.train_1.config(text=str(self.people_dict['train_1']))
                     self.gui_mod.train_2.config(text=str(self.people_dict['train_2']))
                     self.gui_mod.train_3.config(text=str(self.people_dict['train_3']))
+
+                    if self.gui_mod.file_var.get():
+                        self.save_station_state()
+                        self.row_num += 1
+
                     print('Посадка. Загруженность вокзала:'
                           '\n     - платформа 1: ', self.people_dict['platform_1'],
                           '\n     - платформа 2: ', self.people_dict['platform_2'],
@@ -214,3 +263,4 @@ class StationProvider(Thread):
 
     def quit(self):
         self.alive = False
+        self.excel_file.save(''.join(['data/', datetime.strftime(datetime.today(), "%H-%M-%S"), '.xlsx']))
